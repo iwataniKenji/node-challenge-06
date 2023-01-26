cde620f94e84c103e733d9929933f97e3dd38c41
iwatani-kenji <kleversonkenji@gmail.com>
2023-01-25 22:09:34 -0300

    finishes ignite challenge #6

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,32 @@
+import { User } from "./../../../users/entities/User";
+import { Game } from "./../../entities/Game";
+import { getRepository, Repository } from "typeorm";
+
+import { IGamesRepository } from "../IGamesRepository";
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return this.repository
+      .createQueryBuilder()
+      .where("title ILIKE :title", { title: `%${param}%` })
+      .getMany();
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query("SELECT COUNT(*) FROM games");
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return this.repository
+      .createQueryBuilder()
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
+  }
+}
